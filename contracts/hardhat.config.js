c800354b011d3268e945eda18810eaf09f3f13d7
jsan <ahsan.wtc@gmail.com>
2022-08-26 13:45:28 +0200

    setting up the project

---
+++ contracts/hardhat.config.js
@@ -0,0 +1,60 @@
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
+require("@nomiclabs/hardhat-ethers");
+require("dotenv").config({ path: __dirname + "/.env" });
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.10"
+      }
+    ]
+  },
+  // defaultNetwork: "dev",
+  networks: {
+    dev: {
+      url: "http://localhost:7545",
+      gasPrice: 20,
+      accounts: {
+        mnemonic: process.env.MNEMONIC,
+        count: 10
+      },
+      saveDeployments: true
+    },
+    bsctest: {
+      url: "https://data-seed-prebsc-2-s2.binance.org:8545",
+      accounts: [process.env.PRIVATE_KEY],
+      gasPrice: 10000000000,
+      blockGasLimit: 1000000
+    },
+    solanartest: {
+      url: "https://api.testnet.solana.com",
+      accounts: [process.env.PRIVATE_KEY],
+      gasPrice: 10000000000,
+      blockGasLimit: 10000000
+    },
+    polygontest: {
+      url: "https://rpc-mumbai.maticvigil.com",
+      accounts: [process.env.PRIVATE_KEY],
+      gasPrice: 10000000000,
+      blockGasLimit: 10000000
+    },
+    main: {
+      url: "https://bsc-dataseed1.binance.org",
+      accounts: [process.env.PRIVATE_KEY],
+      gasPrice: 5100000000,
+      blockGasLimit: 1000000
+    }
+  },
+
+  etherscan: {
+    apiKey: process.env.API_KEY
+  }
+};
